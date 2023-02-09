84e5f5138cab5ba47f581a49f1d5905ac1b0572e
r0nny8000 <r0nny8000@bea2b.de>
2023-02-09 23:03:47 +0100

    get account balances

--- crypto/api/binance.py
+++ crypto/api/binance.py
@@ -1,5 +1,21 @@
+from binance import Client
+
+
+def printAssetBalance(asset):
+    name = asset['asset']
+    balance = asset['free'].rjust(16)
+    print(name + ":\t" + balance)
 
 
 
-def test():
-    print("test")
+def account(api_key, api_sec):
+    client = Client(api_key, api_sec, {"timeout": 8})
+    
+    
+
+    printAssetBalance(client.get_asset_balance(asset='USDT'))
+    printAssetBalance(client.get_asset_balance(asset='BTC'))
+    printAssetBalance(client.get_asset_balance(asset='ETH'))
+
+
+    print()
--- crypto/cry.py
+++ crypto/cry.py
@@ -1,17 +1,29 @@
 """
-cli
+Command Line Interface for CRY
 
 """
+import os
 import fire
 
+from pyfiglet import Figlet
+
 import crypto.api.binance as api
+
+
+BINANCE_API_KEY = os.path.expanduser(os.getenv('BINANCE_API_KEY','NONE'))
+BINANCE_API_SEC = os.path.expanduser(os.getenv('BINANCE_API_SEC','NONE'))
+
+def _printLarge(msg):
+    figlet = Figlet(font='cyberlarge')
+    print(figlet.renderText(msg))
+
 
 def wallet():
     """
     show balances of the wallet
     """
-    api.test()
-
+    _printLarge("Account")
+    api.account(BINANCE_API_KEY, BINANCE_API_SEC)
 
 def _main():
     fire.Fire()
