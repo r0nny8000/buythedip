5d06c28df0790cd6c8bd655e6c37c3d55f6395a8
r0nny8000 <r0nny8000@bea2b.de>
2023-02-11 23:58:31 +0100

    first working version with exchange

--- crypto/api/binance.py
+++ crypto/api/binance.py
@@ -1,4 +1,5 @@
 from binance import Client
+from binance import exceptions 
 
 
 def getClient(api_key, api_sec):
@@ -7,12 +8,10 @@ def getClient(api_key, api_sec):
 def printAssetBalance(asset):
     name = asset['asset']
     balance = asset['free'].rjust(16)
-    print(name + ":\t" + balance)
+    print(name + "\t" + balance)
 
 def printSymbol(symbol):
-    name = symbol['symbol']
-    price = symbol['price'].rjust(16) + " USD"
-    print(name + ":\t" + price)
+    print(symbol['symbol'] + ":\t" + str(float(symbol['price'])).rjust(16) + " USDT")
 
 
 
@@ -21,7 +20,33 @@ def account(api_key, api_sec):
     printAssetBalance(client.get_asset_balance(asset='USDT'))
     printAssetBalance(client.get_asset_balance(asset='BTC'))
     printAssetBalance(client.get_asset_balance(asset='ETH'))
+    
+
+def exchange(api_key, api_sec, from_quantity, from_symbol, to_symbol):
+    client = getClient(api_key,api_sec)
+    symbol = to_symbol + from_symbol
+
+    print("Balance before exchange:\n")
+    account(api_key, api_sec)
     print()
+
+    try:
+        price = client.get_symbol_ticker(symbol=symbol)
+        print("Exchange:\n")
+        print(from_quantity + " " + from_symbol + " to " + to_symbol + " at a rate of " + str(float(price['price'])) + " " + from_symbol)
+
+        to_quantity = round(float(from_quantity) / float(price['price']), 4)
+        print(str(to_quantity) + " " + to_symbol + " calculated")
+
+        order = client.order_market_buy(symbol=symbol, quantity=to_quantity)
+        print(str(float(order['executedQty'])) + " " + to_symbol + " received")
+
+    except exceptions.BinanceAPIException as exception:
+        print(exception)
+
+    print()
+    print("Balance after exchange:\n")
+    account(api_key, api_sec)
 
 
 def prices(api_key, api_sec):
--- crypto/cry.py
+++ crypto/cry.py
@@ -3,8 +3,7 @@ Command Line Interface for CRY
 
 """
 import os
-import fire
-
+import click
 from pyfiglet import Figlet
 
 import crypto.api.binance as api
@@ -13,28 +12,41 @@ import crypto.api.binance as api
 BINANCE_API_KEY = os.path.expanduser(os.getenv('BINANCE_API_KEY','NONE'))
 BINANCE_API_SEC = os.path.expanduser(os.getenv('BINANCE_API_SEC','NONE'))
 
-def _printLarge(msg):
+@click.group()
+def cli():
+    pass
+
+def printHeadline(msg):
     figlet = Figlet(font='cyberlarge')
     print(figlet.renderText(msg))
 
-
+@cli.command()
 def account():
     """
-    show balances of the wallet
+    Show the current balances of the wallet.
     """
-    _printLarge("Account")
+    printHeadline("Account")
     api.account(BINANCE_API_KEY, BINANCE_API_SEC)
 
+@cli.command()
 def prices():
     """
-    show prices
+    Show the prices of Cryptos.
     """
-    _printLarge("Prices")
+    printHeadline("Prices")
     api.prices(BINANCE_API_KEY, BINANCE_API_SEC)
 
+@cli.command()
+@click.argument('amount')
+@click.argument('from_symbol')
+@click.argument('to_symbol')
+def exchange(amount, from_symbol, to_symbol):
+    """
+    Exchange an <amount> of crypto from <symbol> to <symbol>.
+    """
+    printHeadline("Exchange")
+    api.exchange(BINANCE_API_KEY, BINANCE_API_SEC, amount, from_symbol, to_symbol)
 
-def _main():
-    fire.Fire()
 
 if __name__ == '__main__':
-    _main()
+    cli()    
